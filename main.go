cb6ed74a454b8f1564cc6993b04e329508e79cf4
Alexandre <alexandre.suriano@gmail.com>
2021-12-17 00:58:24 -0300

    switch main URL to search news

--- main.go
+++ main.go
@@ -2,47 +2,67 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
-
-	"github.com/antchfx/htmlquery"
 )
 
 func main() {
 
-	url := "https://www.jcnet.com.br/noticias/ultimas_noticias"
-	var valor string
+	var allNews string
 
-	doc, err := htmlquery.LoadURL(url)
-	if err != nil {
-		log.Fatal(err)
+	for i := 1; i <= 5; i++ {
+		allNews += getNews(i)
 	}
 
-	//list, err := htmlquery.QueryAll(doc, "//a")
-	list, err := htmlquery.QueryAll(doc, "//a")
-	if err != nil {
-		log.Fatal(err)
+	errFile := os.WriteFile("news.html", []byte(allNews), 0644)
+	if errFile != nil {
+		log.Panic(errFile)
 	}
 
-	for i, n := range list {
-		a := htmlquery.FindOne(n, "//a")
-		if a != nil {
-			valor += fmt.Sprintf("%d %s(%s)\n", i, htmlquery.InnerText(a), htmlquery.SelectAttr(a, "href"))
-		}
-	}
-
-	fmt.Println(valor)
-
-	os.WriteFile("JCNET.txt", []byte(valor), 0644)
 }
 
-// <li class="loadMoreItem">
-// <a href="/servicos/falecimentos/2021/10/778574-cacilda-cardamone-camargo.html">
-// <div class="site">Falecimentos</div>
-// <h3>
-// CACILDA CARDAMONE CAMARGO </h3>
-// <p class="linha-fina">
-// Ocorrido em 21/10/2021, com 89 anos. Era viúva do sr JOÃO GOMES DE CAMARGO e deixa os filhos MARA L... </p>
-// <div class="dt">21/10/2021 19:35 - <time class="timeago" datetime="2021-10-21T19:35:16Z" title="21/10/2021 19:35">há 7 horas</time></div>
-// </a>
-// </li>
+func getNews(pageNumber int) string {
+	//url := fmt.Sprint("https://www.jcnet.com.br/webparts.php?sesit=5&site=25&file=%252Fapp%252Fview%252Fparts%252Fpart-listao&p=", pageNumber)
+	url := fmt.Sprint("https://www.jcnet.com.br/webparts.php?sesit=5&site=25&file=%2Fapp%2Fview%2Fparts%2Fpart-listao&p=", pageNumber)
+
+	fmt.Println(url)
+
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		log.Panic(err)
+	}
+	req.Header.Add("authority", "www.jcnet.com.br")
+	req.Header.Add("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Google Chrome\";v=\"96\"")
+	req.Header.Add("accept", "text/html, */*; q=0.01")
+	req.Header.Add("x-requested-with", "XMLHttpRequest")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")
+	req.Header.Add("sec-ch-ua-platform", "\"Windows\"")
+	req.Header.Add("sec-fetch-site", "same-origin")
+	req.Header.Add("sec-fetch-mode", "cors")
+	req.Header.Add("sec-fetch-dest", "empty")
+	req.Header.Add("referer", "https://www.jcnet.com.br/noticias/ultimas_noticias")
+	req.Header.Add("accept-language", "pt,pt-BR;q=0.9,en;q=0.8")
+	req.Header.Add("cookie", "_ga=GA1.3.1210185455.1639703925; _gid=GA1.3.802578550.1639703925; _fw_plugins=1196662060; _fw_fonts=459217059; _fw_clientIp=179.127.52.121; _fw_validIP=true; evercookie_png=61bbe57944b64; evercookie_etag=61bbe57944b64; evercookie_cache=61bbe57944b64; _everfw4=61bbe57944b64; _everfw4=61bbe57944b64; _fw_views=1; cookieconsent_status=dismiss; paywall=true; _fw_userName=; _fw_signId=; _fw_email=; _fw_plan=; _fw_planId=; _fw_planValid=false; _dc_gtm_UA-24117260-1=1; _gat_gtag_UA_175164381_36=1")
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer res.Body.Close()
+
+	//fmt.Println(res)
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return string(body)
+}
